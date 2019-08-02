3d5cf7079754b6c3fa437ed2ba837c3a391e39ae
RealgodJJ <xiaozhiqiu13@sina.com>
2019-08-02 20:18:18 +0800

    Compare the complexity of linkListMap and BSTMap.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,47 @@
 import java.util.ArrayList;
 
 public class Main {
+    private static double testWord(String fileName, Map<String, Integer> wordMap) {
+        double startTime = System.nanoTime();
 
-    public static void main(String[] args) {
         System.out.println("Pride and Prejudice");
         ArrayList<String> words = new ArrayList<>();
 
+        if (FileOperation.readFile("src/article/pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+
+            for (String word : words) {
+                if (wordMap.contains(word))
+                    wordMap.set(word, wordMap.get(word) + 1);
+                else
+                    wordMap.add(word, 1);
+            }
+
+            System.out.println("Total different words: " + wordMap.getSize());
+            System.out.println("Frequency of PRIDE: " + wordMap.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + wordMap.get("prejudice"));
+        }
+
+        double endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        BSTMap<String, Integer> bstMap = new BSTMap<>();
+        double time1 = testWord("src/article/pride-and-prejudice.txt", bstMap);
+        System.out.println("bstMap: " + time1 + "s");
+
+        System.out.println();
+
+        LinkedListMap<String, Integer> linkedListMap = new LinkedListMap<>();
+        double time2 = testWord("src/article/pride-and-prejudice.txt", linkedListMap);
+        System.out.println("linkedListMap: " + time2 + "s");
+
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words = new ArrayList<>();
+
+
+        System.out.println("============================");
         if (FileOperation.readFile("src/article/pride-and-prejudice.txt", words)) {
             System.out.println("Total words: " + words.size());
 
